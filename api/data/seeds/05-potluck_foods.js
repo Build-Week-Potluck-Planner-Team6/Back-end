d1c6df3e6b608ba96dc5caea62a47f79b0bedd79
Raj-04 <1204patel.raj@gmail.com>
2021-12-21 12:59:46 -0500

    potluck_food seeds

---
+++ api/data/seeds/05-potluck_foods.js
@@ -0,0 +1,30 @@
+exports.seed = function(knex) {
+  return knex('potluck_foods').del()
+    .then(function () {
+      return knex('potluck_foods').insert([
+        {
+          potluck_id: '3',
+          food_id: '2',
+          assigned_user: 7,
+        },
+        {
+          potluck_id: '2',
+          food_id: '5',
+          assigned_user: 4,
+        },
+        {
+          potluck_id: '1',
+          food_id: '4',
+        },
+        {
+          potluck_id: '3',
+          food_id: '1',
+          assigned_user: 4,
+        },
+        {
+          potluck_id: '3',
+          food_id: '6',
+        },
+      ])
+    })
+}
