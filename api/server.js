70c1ba9991419d273386d9e30f57a66fa660ae84
Raj-04 <1204patel.raj@gmail.com>
2021-12-21 14:09:36 -0500

    server file

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
+const potlucksRouter = require('./potlucks/potlucks-router')
+const foodsRouter = require('./foods/foods-router')
+
+const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
+server.use('/api/potlucks', potlucksRouter)
+server.use('/api/foods', foodsRouter)
+
+server.get('/', (req, res, next) => { // eslint-disable-line
+  res.send('<h1>Here\'s the <a href="">README</a></h1>')
+})
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
+module.exports = server
